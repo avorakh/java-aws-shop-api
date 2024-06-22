4d987728a5cfcb0f5a933f3ce3d0916efc22305e
Aliaksandr Vorakh <aliaksandrvorakh@gmail.com>
2024-06-22 17:02:51 +0200

    Modify the 'get-products-list-function' module

--- get-products-list-function/src/main/java/dev/avorakh/shop/function/LambdaHandler.java
+++ get-products-list-function/src/main/java/dev/avorakh/shop/function/LambdaHandler.java
@@ -5,14 +5,33 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
 import com.amazonaws.services.lambda.runtime.logging.LogLevel;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.avorakh.shop.dao.DynamoDbProductDao;
+import dev.avorakh.shop.dao.DynamoDbStockDao;
 import dev.avorakh.shop.function.model.CommonUtils;
-import dev.avorakh.shop.function.model.MockData;
+import dev.avorakh.shop.svc.DefaultProductService;
+import dev.avorakh.shop.svc.ProductService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class LambdaHandler implements RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
 
     private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    ProductService productService;
+
+    public LambdaHandler() {
+        var dynamoDbClient = DynamoDbClient.builder().build();
+        var productTableName = System.getenv("PRODUCT_TABLE_NAME"); // "Product";
+        var stockTableName = System.getenv("STOCK_TABLE_NAME"); // "Stock";
+        var productDao = new DynamoDbProductDao(dynamoDbClient, productTableName);
+        var stockDao = new DynamoDbStockDao(dynamoDbClient, stockTableName);
+        this.productService = new DefaultProductService(productDao, stockDao);
+    }
 
     @Override
     public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent input, Context context) {
@@ -21,10 +40,11 @@ public class LambdaHandler implements RequestHandler<APIGatewayV2HTTPEvent, APIG
             var inputEvent = objectMapper.writeValueAsString(input);
 
             logger.log("LambdaHandler call input - [%s]".formatted(inputEvent), LogLevel.INFO);
-            var body = objectMapper.writeValueAsString(MockData.mockProductMap.values());
+
+            var body = objectMapper.writeValueAsString(productService.getAll());
 
             return CommonUtils.toAPIGatewayV2HTTPResponse(200, body);
-        } catch (JsonProcessingException e) {
+        } catch (Exception e) {
             logger.log(e.getMessage(), LogLevel.ERROR);
             return CommonUtils.toErrorAPIGatewayV2HTTPResponse(500, "INTERNAL_SERVER_ERROR", 1000);
         }
