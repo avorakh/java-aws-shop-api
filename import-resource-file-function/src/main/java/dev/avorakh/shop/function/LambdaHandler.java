7870e005d8a97b906ca922cafc0c336804a3cfc3
Aliaksandr Vorakh <aliaksandrvorakh@gmail.com>
2024-06-29 01:22:20 +0200

    Add 'import-resource-file-function'

--- import-resource-file-function/src/main/java/dev/avorakh/shop/function/LambdaHandler.java
+++ import-resource-file-function/src/main/java/dev/avorakh/shop/function/LambdaHandler.java
@@ -6,37 +6,35 @@ import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
 import com.amazonaws.services.lambda.runtime.logging.LogLevel;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import dev.avorakh.shop.dao.DynamoDbProductDao;
-import dev.avorakh.shop.dao.DynamoDbStockDao;
 import dev.avorakh.shop.function.model.CommonUtils;
 import dev.avorakh.shop.function.model.ErrorDetail;
 import dev.avorakh.shop.function.model.ErrorResource;
-import dev.avorakh.shop.svc.DefaultProductService;
-import dev.avorakh.shop.svc.ProductService;
+import java.time.Duration;
 import java.util.List;
+import java.util.Map;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.apache.commons.lang3.StringUtils;
-import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class LambdaHandler implements RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
 
-    private static final String VALIDATION_ERROR = "VALIDATION_ERROR";
-
     private static final ObjectMapper objectMapper = new ObjectMapper();
 
-    ProductService productService;
+    S3Presigner s3Presigner;
+    String bucketName;
+    String uploadFolder;
+    Long expirationSeconds;
 
     public LambdaHandler() {
-        var dynamoDbClient = DynamoDbClient.builder().build();
-        var productTableName = System.getenv("PRODUCT_TABLE_NAME");
-        var stockTableName = System.getenv("STOCK_TABLE_NAME");
-        var productDao = new DynamoDbProductDao(dynamoDbClient, productTableName);
-        var stockDao = new DynamoDbStockDao(dynamoDbClient, stockTableName);
-        this.productService = new DefaultProductService(productDao, stockDao);
+        this.s3Presigner = S3Presigner.builder().build();
+        this.bucketName = System.getenv("UPLOAD_BUCKET");
+        this.uploadFolder = System.getenv("UPLOAD_FOLDER");
+        this.expirationSeconds = Long.parseLong(System.getenv("EXPIRATION_SECONDS"));
     }
 
     @Override
@@ -47,30 +45,37 @@ public class LambdaHandler implements RequestHandler<APIGatewayV2HTTPEvent, APIG
 
             logger.log("LambdaHandler call input - [%s]".formatted(inputEvent), LogLevel.INFO);
 
-            String id = input.getPathParameters().get("productId");
-            logger.log("productId - [%s]".formatted(id), LogLevel.INFO);
+            String name = input.getQueryStringParameters().get("name");
+            logger.log("name - [%s]".formatted(name), LogLevel.INFO);
 
-            if (StringUtils.isBlank(id)) {
-                var validationError = new ErrorDetail("the productId path parameter should be present");
-                var errorResource = new ErrorResource(VALIDATION_ERROR, 1001, List.of(validationError));
+            if (StringUtils.isBlank(name)) {
+                var validationError = new ErrorDetail("the 'name' query parameter should be present");
+                var errorResource = new ErrorResource("VALIDATION_ERROR", 1002, List.of(validationError));
                 var body = objectMapper.writeValueAsString(errorResource);
                 return CommonUtils.toAPIGatewayV2HTTPResponse(400, body);
             }
 
-            var foundProduct = productService.get(id);
+            String key = uploadFolder + "/" + name;
+            String uploadUrl = createPresignedUrl(key);
 
-            if (foundProduct.isPresent()) {
-                var body = objectMapper.writeValueAsString(foundProduct.get());
-                return CommonUtils.toAPIGatewayV2HTTPResponse(200, body);
-            }
+            logger.log("uploadUrl - [%s]".formatted(uploadUrl), LogLevel.INFO);
 
-            logger.log("Product not found", LogLevel.ERROR);
+            var body = objectMapper.writeValueAsString(Map.of("uploadUrl", uploadUrl));
 
-            return CommonUtils.toErrorAPIGatewayV2HTTPResponse(404, "PRODUCT_NOT_EXIST", 1100);
+            return CommonUtils.toAPIGatewayV2HTTPResponse(200, body);
         } catch (Exception e) {
             logger.log(e.getMessage(), LogLevel.ERROR);
-
             return CommonUtils.toErrorAPIGatewayV2HTTPResponse(500, "INTERNAL_SERVER_ERROR", 1000);
         }
+    }
+
+    private String createPresignedUrl(String key) {
+        var objectRequest =
+                PutObjectRequest.builder().bucket(bucketName).key(key).build();
+
+        var presignedRequest = s3Presigner.presignPutObject(
+                r -> r.putObjectRequest(objectRequest).signatureDuration(Duration.ofSeconds(expirationSeconds)));
+
+        return presignedRequest.url().toString();
     }
 }
