ca3d64327c44715ce2a02d97e8a2c2df107fc600
Aliaksandr Vorakh <aliaksandrvorakh@gmail.com>
2024-06-29 01:38:59 +0200

    Integrate importProductsFile function in the Import Service CDK project

--- cdk-import-svc-java/src/main/java/dev/avorakh/shop/cdk/MyShopImportServiceBackendJavaStack.java
+++ cdk-import-svc-java/src/main/java/dev/avorakh/shop/cdk/MyShopImportServiceBackendJavaStack.java
@@ -5,6 +5,7 @@ import org.jetbrains.annotations.Nullable;
 import software.amazon.awscdk.*;
 import software.amazon.awscdk.services.apigateway.*;
 import software.amazon.awscdk.services.lambda.Code;
+import software.amazon.awscdk.services.lambda.Function;
 import software.amazon.awscdk.services.lambda.FunctionProps;
 import software.amazon.awscdk.services.lambda.Runtime;
 import software.amazon.awscdk.services.s3.BlockPublicAccess;
@@ -14,23 +15,76 @@ import software.amazon.awscdk.services.s3.HttpMethods;
 import software.constructs.Construct;
 
 import java.util.List;
+import java.util.Map;
 
 public class MyShopImportServiceBackendJavaStack extends Stack {
+
+    public static final String APPLICATION_JSON = "application/json";
 
     public MyShopImportServiceBackendJavaStack(@Nullable Construct scope, @Nullable String id) {
         this(scope, id, null);
     }
 
-
     public MyShopImportServiceBackendJavaStack(@Nullable Construct scope, @Nullable String id, @Nullable StackProps props) {
         super(scope, id, props);
-        var s3Bucket = createBucket();
 
+        String bucketName = "avorakh-my-shop-import-svc-cdk";
+
+        var s3Bucket = createBucket(bucketName);
+
+        String importProductsFileFunctionName = "importProductsFile";
+
+        var importProductsFileFunction = new Function(
+                this,
+                importProductsFileFunctionName,
+                getLambdaFunctionProps(
+                        importProductsFileFunctionName,
+                        "./../asset/import-resource-file-function-aws.jar",
+                        "dev.avorakh.shop.function.LambdaHandler::handleRequest"));
+
+        // Add Environment Variables to 'importProductsFile' Lambda function
+        importProductsFileFunction.addEnvironment("UPLOAD_BUCKET", bucketName);
+        importProductsFileFunction.addEnvironment("UPLOAD_FOLDER", "uploaded");
+        importProductsFileFunction.addEnvironment("EXPIRATION_SECONDS", "600");
+
+        s3Bucket.grantPut(importProductsFileFunction);
+
+        var api = createApiGateway();
+
+        var importResource = api.getRoot().addResource("import");
+
+        var importProductsFile = new LambdaIntegration(importProductsFileFunction);
+
+        var preSignedURLResponse = Model.Builder.create(this, "PreSignedURLResponse")
+                .restApi(api)
+                .schema(JsonSchema.builder()
+                        .schema(JsonSchemaVersion.DRAFT4)
+                        .type(JsonSchemaType.STRING)
+                        .build())
+                .description("Returns Pre-Signed URL to upload CSV")
+                .contentType(APPLICATION_JSON)
+                .build();
+
+        importResource.addMethod("GET", importProductsFile, MethodOptions.builder()
+                .methodResponses(List.of(
+                        MethodResponse.builder()
+                                .statusCode("200")
+                                .responseModels(Map.of(APPLICATION_JSON, preSignedURLResponse))
+                                .build(),
+                        MethodResponse.builder()
+                                .statusCode("500")
+                                .responseModels(Map.of(APPLICATION_JSON, Model.ERROR_MODEL))
+                                .build()))
+                .requestParameters(Map.of("method.request.querystring.name", true))
+                .requestValidatorOptions(RequestValidatorOptions.builder()
+                        .validateRequestParameters(true)
+                        .build())
+                .build());
     }
 
-    private @NotNull Bucket createBucket() {
+    private @NotNull Bucket createBucket(String bucketName) {
         return Bucket.Builder.create(this, "CsvImportServiceBucket")
-                .bucketName("avorakh-my-shop-import-svc-cdk")
+                .bucketName(bucketName)
                 .blockPublicAccess(BlockPublicAccess.BLOCK_ALL)
                 .cors(List.of(CorsRule.builder()
                         .allowedMethods(List.of(HttpMethods.GET, HttpMethods.PUT))
